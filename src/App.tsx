fecd92a602b4930ce77d62a7005f0807b6017f4e
Edwin Ekpenyong <moogiemode@gmail.com>
2021-07-23 22:48:39 -0700

    eslint partially configured

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { FC } from 'react';
 
 import './assets/index.scss';
 
-const App: React.FC<{}> = () => <div>React/TypeScript is Rendering</div>;
+let apple: any;
+
+const App: FC = () => <div>React/TypeScript is Rendering</div>;
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from "react";
-import ReactDOM from "react-dom";
+import React, { FC } from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./App";
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
